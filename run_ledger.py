43fa04b80ecbc97eb0f865954094dcd3454bfce9
Stathis <emoraitidis@gmail.com>
2020-11-04 00:21:22 +0000

    Use days instead of months

--- run_ledger.py
+++ run_ledger.py
@@ -9,7 +9,7 @@ ledger.load_transactions()
 
 current_balance = ledger.get_current_balance("john")
 
-one_month_ago = datetime.date.today() - datetime.timedelta(months=30)
+one_month_ago = datetime.date.today() - datetime.timedelta(days=30)
 one_month_ago_balance = ledger.get_balance_at(one_month_ago, "john")
 
 print("Current balance:", current_balance)
