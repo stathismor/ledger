6f19061dda2ae52587d2faecd2e71c46f2b97c49
Stathis <emoraitidis@gmail.com>
2020-11-04 00:38:55 +0000

    Show balance two months ago

--- run_ledger.py
+++ run_ledger.py
@@ -9,8 +9,8 @@ ledger.load_transactions()
 
 current_balance = ledger.get_current_balance("john")
 
-one_month_ago = datetime.date.today() - datetime.timedelta(days=30)
-one_month_ago_balance = ledger.get_balance_at(one_month_ago, "john")
+one_month_ago = datetime.date.today() - datetime.timedelta(days=60)
+two_month_ago_balance = ledger.get_balance_at(one_month_ago, "john")
 
 print("Current balance:", current_balance)
-print("Balance one month ago:", one_month_ago_balance)
+print("Balance one month ago:", two_month_ago_balance)
